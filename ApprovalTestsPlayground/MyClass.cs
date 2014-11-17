cc24b33f5e4267436c5fe6e439be3338386feaa3
John Keith <johnkeith16@gmail.com>
2014-11-16 22:29:18 -0500

    Added a web service interface.

--- ApprovalTestsPlayground/MyClass.cs
+++ ApprovalTestsPlayground/MyClass.cs
@@ -5,19 +5,35 @@ namespace ApprovalTestsPlayground
 	public interface ILog
 	{
 		void Debug (string message);
-		new string ToString ();
+		void Error (string message);
+	}
+
+	public interface IWebService
+	{
+		string GetMultiplier ();
 	}
 
 	public class MyClass
 	{
-		public string DoWeirdMath (int a, int b, ILog log)
+		public string DoWeirdMath (int a, int b, ILog log, IWebService webService)
 		{
 			log.Debug ("Entered method...");
 
 			if (b == 0)
 				throw new ArgumentException ("Zero for b? You must be kidding me!");
 
-			var result = 3 * a + 2 * b + a / b - 4;
+			int multiplier = 0;
+			try
+			{
+				var multiplierText = webService.GetMultiplier ();
+				int.TryParse (multiplierText, out multiplier);
+			}
+			catch (Exception ex)
+			{
+				log.Error ("Web service call failed. " + ex.ToString ());
+			}
+
+			var result = (3 * a + 2 * b + a / b - 4) * multiplier;
 
 			log.Debug("Leaving method...");
 
--- ApprovalTestsPlaygroundUnitTest/MyClassTest.cs
+++ ApprovalTestsPlaygroundUnitTest/MyClassTest.cs
@@ -22,6 +22,24 @@ namespace ApprovalTestsPlaygroundUnitTest
 			{
 				throw new NotImplementedException();
 			}
+
+			public virtual void Error(string message)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public class WebServiceWithToStringOverride : IWebService
+		{
+			public override string ToString ()
+			{
+				throw new NotImplementedException();
+			}
+
+			public virtual string GetMultiplier ()
+			{
+				throw new NotImplementedException();
+			}
 		}
 
 		[Test]
@@ -30,12 +48,50 @@ namespace ApprovalTestsPlaygroundUnitTest
 			var sut = new MyClass ();
 			var firsts = new[] { -10, -5, 0, 5, 10 };
 			var seconds = new[] { -2, -1, 0, 1, 2 };
-			var mockLog = MockRepository.GenerateMock<LogWithToStringOverride> ();
-			mockLog.Stub (x => x.ToString ()).Return ("mock log").Repeat.Any();
-			var logs = new[] { mockLog };
+			var logs = new[] {
+				CreateMockLog("mock log")
+			};
+			var webServices = new[] {
+				CreateMockWebServiceReturn ("-10"),
+				CreateMockWebServiceReturn ("0"),
+				CreateMockWebServiceReturn ("10"),
+				CreateMockWebServiceReturn ("something unexpected"),
+				CreateMockWebServiceReturn (null),
+				CreateMockWebServiceException ("some exception")
+			};
 
 			// Act and assert
-			CombinationApprovals.VerifyAllCombinations (sut.DoWeirdMath, firsts, seconds, logs);
+			CombinationApprovals.VerifyAllCombinations (sut.DoWeirdMath, firsts, seconds, logs, webServices);
+		}
+
+		private static LogWithToStringOverride CreateMockLog (string toString)
+		{
+			var mockLog = MockRepository.GenerateMock<LogWithToStringOverride> ();
+			StubToString (mockLog, toString);
+			mockLog.Stub (x => x.ToString ()).Return (toString);
+			return mockLog;
+		}
+
+		private static WebServiceWithToStringOverride CreateMockWebServiceReturn (string result)
+		{
+			var mockWebService = MockRepository.GenerateMock<WebServiceWithToStringOverride> ();
+			StubToString (mockWebService, result);
+			mockWebService.Stub (x => x.GetMultiplier ()).Return (result);
+			return mockWebService;
+		}
+
+		private static WebServiceWithToStringOverride CreateMockWebServiceException (string message)
+		{
+			var mockWebService = MockRepository.GenerateMock<WebServiceWithToStringOverride> ();
+			StubToString (mockWebService, message);
+			mockWebService.Stub (x => x.GetMultiplier ()).Throw(new Exception(message));
+			return mockWebService;
+		}
+
+		private static void StubToString(object obj, string toString)
+		{
+			var nonNullToString = toString == null ? "null" : toString;
+			obj.Stub (x => x.ToString ()).Return (nonNullToString);
 		}
 	}
 }
