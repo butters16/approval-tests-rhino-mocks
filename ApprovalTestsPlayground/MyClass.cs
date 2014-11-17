b2721e097f9fda97fe3ef64584936e2d2e652487
John Keith <johnkeith16@gmail.com>
2014-11-16 21:46:17 -0500

    Added an interface and stubbed ToString for it.

--- ApprovalTestsPlayground/MyClass.cs
+++ ApprovalTestsPlayground/MyClass.cs
@@ -2,14 +2,26 @@ using System;
 
 namespace ApprovalTestsPlayground
 {
+	public interface ILog
+	{
+		void Debug (string message);
+		new string ToString ();
+	}
+
 	public class MyClass
 	{
-		public string DoWeirdMath (int a, int b)
+		public string DoWeirdMath (int a, int b, ILog log)
 		{
+			log.Debug ("Entered method...");
+
 			if (b == 0)
 				throw new ArgumentException ("Zero for b? You must be kidding me!");
 
-			return (3 * a + 2 * b + a / b - 4).ToString();
+			var result = 3 * a + 2 * b + a / b - 4;
+
+			log.Debug("Leaving method...");
+
+			return (result).ToString();
 		}
 	}
 }
--- ApprovalTestsPlaygroundUnitTest/MyClassTest.cs
+++ ApprovalTestsPlaygroundUnitTest/MyClassTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using ApprovalTestsPlayground;
 using ApprovalTests.Combinations;
 using ApprovalTests.Reporters;
+using Rhino.Mocks;
 
 namespace ApprovalTestsPlaygroundUnitTest
 {
@@ -10,14 +11,31 @@ namespace ApprovalTestsPlaygroundUnitTest
 	[UseReporter(typeof(FileLauncherReporter))]
 	public class MyClassTest
 	{
+		public class LogWithToStringOverride : ILog
+		{
+			public override string ToString ()
+			{
+				throw new NotImplementedException();
+			}
+
+			public virtual void Debug(string message)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
 		[Test]
 		public void Test ()
 		{
 			var sut = new MyClass ();
 			var firsts = new[] { -10, -5, 0, 5, 10 };
 			var seconds = new[] { -2, -1, 0, 1, 2 };
+			var mockLog = MockRepository.GenerateMock<LogWithToStringOverride> ();
+			mockLog.Stub (x => x.ToString ()).Return ("mock log").Repeat.Any();
+			var logs = new[] { mockLog };
 
-			CombinationApprovals.VerifyAllCombinations (sut.DoWeirdMath, firsts, seconds);
+			// Act and assert
+			CombinationApprovals.VerifyAllCombinations (sut.DoWeirdMath, firsts, seconds, logs);
 		}
 	}
 }
